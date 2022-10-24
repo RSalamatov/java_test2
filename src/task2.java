579f46ae415f4a530efb9814c4ba4975a4e65016
RSalamatov <salamatov.roman7030@gmail.com>
2022-10-24 20:51:14 +0400

    Выполнено задание №2 типы данных

---
+++ src/task2.java
@@ -0,0 +1,9 @@
+public class task2 {
+    public static void main(String[] args) {
+        double a = 3.1416;
+        byte b = 127;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
