d46c4f0aab8ac743fbc68ffb95f3821b6563e804
RSalamatov <salamatov.roman7030@gmail.com>
2022-10-25 11:33:54 +0400

    Выполнено задание №3 типы данных

---
+++ src/task3.java
@@ -0,0 +1,12 @@
+public class task3 {
+    public static void main(String[] args) {
+        int [] num = {460, 4, 22, 12, 3};
+
+        int last = num[num.length - 1];
+        int first = num[0];
+        num[0]=last;
+        num[num.length - 1]=first;
+        System.out.println(num[0]+num[num.length-3]);
+
+    }
+}
