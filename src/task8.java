88f3bfabfda9bada059c05c721bbdc55dca434e6
RSalamatov <salamatov.roman7030@gmail.com>
2023-01-17 13:59:00 +0400

    Выполнено задание №8 Циклы

---
+++ src/task8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class task8 {
+    public static void main(String[] args) {
+        System.out.println("Ввести любое положительное число n: ");
+        Scanner scr = new Scanner(System.in);
+        int n = scr.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i = i + 2) {
+            sum = sum + i;
+                    }
+        if (n < 0) {
+            System.out.println("Вы ввели отрицательное число");
+        } else {
+            System.out.println("Сумма нечетных чисел = " + sum);
+        }
+    }
+}
+
